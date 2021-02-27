b90adae4f65f188d14e705749918b530ac5bc737
saqzari <zardaris@tcd.ie>
2021-02-27 20:48:57 +0000

    can now accept https requests and connect to the browser and server through transmissions

--- RequestRespond.java
+++ RequestRespond.java
@@ -2,19 +2,23 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class RequestRespond 
+public class RequestRespond implements Runnable 
 {
 	
-	private BufferedReader requestReader;
-	private Socket socket;
+	private BufferedReader proxyToClientReader;
+	private BufferedWriter proxyToClientWriter;
+	private BufferedReader proxyToServerReader;
+	private BufferedWriter proxyToServerWriter;
+	private Socket browserSocket;
 	
 	public RequestRespond(Socket socket)
 	{
-		this.socket = socket;
+		this.browserSocket = socket;
 		try 
 		{
-			InputStreamReader in = new InputStreamReader(socket.getInputStream());
-			requestReader = new BufferedReader(in);
+			browserSocket.setSoTimeout(2000);
+			proxyToClientReader = new BufferedReader(new InputStreamReader(browserSocket.getInputStream()));
+			proxyToClientWriter = new BufferedWriter(new OutputStreamWriter(browserSocket.getOutputStream()));
 			
 		} catch (IOException e) 
 		{
@@ -23,31 +27,83 @@ public class RequestRespond
 		
 	}
 	
-	public void use()
+	@Override
+	public void run() 
 	{
 		String str = null;
 		try {
-			str = requestReader.readLine();
+			str = proxyToClientReader.readLine();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println(str);
 		String[] parts = str.split(" ");
 		String command = parts[0];
 		String url = parts[1];
+
 		
-		if(command.equals("GET"))
+		if(command.equals("CONNECT"))
 		{
-			System.out.println("hello get");
-			System.out.println(url);
-		}
-		else if(command.equals("CONNECT"))
-		{
-			System.out.println("hello connect");
-			System.out.println(url);
+			try {
+			parts = url.split(":");
+			String address = parts[0];
+			int serverPort = Integer.valueOf(parts[1]);
+			
+			for (int i = 0; i < 5; i++) 
+			{
+				proxyToClientReader.readLine();
+			}
+			
+			InetAddress serverAddress;
+			serverAddress = InetAddress.getByName(address);
+			Socket serverSocket = new Socket(serverAddress, serverPort);
+			serverSocket.setSoTimeout(2000);
+			
+			String line = "HTTP/1.0 200 Connection established\r\n" +
+					"Proxy-Agent: ProxyServer/1.0\r\n" +
+					"\r\n";
+			proxyToClientWriter.write(line);
+			proxyToClientWriter.flush();
+			
+			proxyToServerReader = new BufferedReader(new InputStreamReader(browserSocket.getInputStream()));
+			proxyToServerWriter = new BufferedWriter(new OutputStreamWriter(browserSocket.getOutputStream()));
+			
+			transmission(browserSocket, serverSocket);
+			proxyToClientReader.close();
+			proxyToClientWriter.close();
+			proxyToServerReader.close();
+			proxyToServerWriter.close();
+			
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 	
-	
+	public void transmission(Socket browserSocket, Socket serverSocket) throws IOException
+	{
+		final Thread[] threads = new Thread[2];
+		
+		HttpsTransmission clientToServerHttps = new HttpsTransmission(browserSocket.getInputStream(), serverSocket.getOutputStream());
+		HttpsTransmission serverToClientHttps = new HttpsTransmission(serverSocket.getInputStream(), browserSocket.getOutputStream());
+		
+		threads[0] = new Thread(clientToServerHttps);
+		threads[1] = new Thread(serverToClientHttps);
+		
+		threads[0].start();
+		threads[1].start();
+		
+		try { threads[0].join(); threads[1].join(); } catch (InterruptedException e) {}
+	}
 
 }
+
+
